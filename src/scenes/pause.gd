e40a5383e1d60fb8f3edf80125d7c8528ba06636
Super <mmatt2340@gmail.com>
2020-06-01 03:29:11 -0400

    Adjust pause screen to look better

--- src/scenes/pause.gd
+++ src/scenes/pause.gd
@@ -3,10 +3,13 @@ extends Node2D
 
 var can_press = false
 var sound
+var camera
 
 func _ready():
 	sound = get_node("SamplePlayer")
+	camera = get_tree().get_root().get_node("root").find_node("Camera")
 	set_fixed_process(true)
+	
 
 func _fixed_process(delta):
 	if (not Input.is_action_pressed("pause")):
@@ -17,8 +20,11 @@ func _fixed_process(delta):
 		if (get_tree().is_paused()):
 			sound.play("cancel")
 			get_tree().set_pause(false)
+			camera.set("zoom",Vector2(1,1))
 			hide()
 		else:
+			camera.set("zoom",Vector2(0.5,0.5))
 			show()
 			get_tree().set_pause(true)
 			sound.play("accept")
+			
