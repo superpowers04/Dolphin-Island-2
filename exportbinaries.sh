b3138ab7c993c8f140a7fc1b24ecbd6aeb1e123f
Super <mmatt2340@gmail.com>
2020-09-19 06:18:52 -0400

    Export Binaries

--- exportbinaries.sh
+++ exportbinaries.sh
@@ -4,10 +4,10 @@ echo "This script requires godot to be named \`${godotexec}\` in one of your PAT
 #Modify the below to change what the godot executable is
 
 # Debug exports
-${godotexec} -path "${PWD}/src" -export-debug "Linux X11" "${PWD}/bin/DIedit32"
-${godotexec} -path "${PWD}/src" -export-debug "Windows Desktop" "${PWD}/bin/DIedit.exe"
-${godotexec} -path "${PWD}/src" -export-debug "Mac OSX" "${PWD}/bin/Di2Macfat.zip"
-${godotexec} -path "${PWD}/src" -export-debug "Android" "${PWD}/bin/DI2.apk"
+${godotexec} -path "${PWD}/src" -export-debug "Linux X11" "${PWD}/bin/debug/DIedit32"
+${godotexec} -path "${PWD}/src" -export-debug "Windows Desktop" "${PWD}/bin/debug/DIedit.exe"
+${godotexec} -path "${PWD}/src" -export-debug "Mac OSX" "${PWD}/bin/debug/Di2Macfat.zip"
+${godotexec} -path "${PWD}/src" -export-debug "Android" "${PWD}/bin/debug/DI2.apk"
 
 
 # Normal Exports
