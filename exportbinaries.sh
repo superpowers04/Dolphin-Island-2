d4760734eaf40b1687691e66f75ef3130197fcad
Super <mmatt2340@gmail.com>
2020-09-19 05:00:11 -0400

    Add debug builds

--- exportbinaries.sh
+++ exportbinaries.sh
@@ -3,6 +3,14 @@ godotexec="godot216"
 echo "This script requires godot to be named \`${godotexec}\` in one of your PATH folders to work"
 #Modify the below to change what the godot executable is
 
+# Debug exports
+${godotexec} -path "${PWD}/src" -export-debug "Linux X11" "${PWD}/bin/DIedit32"
+${godotexec} -path "${PWD}/src" -export-debug "Windows Desktop" "${PWD}/bin/DIedit.exe"
+${godotexec} -path "${PWD}/src" -export-debug "Mac OSX" "${PWD}/bin/Di2Macfat.zip"
+${godotexec} -path "${PWD}/src" -export-debug "Android" "${PWD}/bin/DI2.apk"
+
+
+# Normal Exports
 
 ${godotexec} -path "${PWD}/src" -export "Linux X11" "${PWD}/bin/DIedit32"
 ${godotexec} -path "${PWD}/src" -export "Windows Desktop" "${PWD}/bin/DIedit.exe"
